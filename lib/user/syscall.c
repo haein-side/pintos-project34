e858c7c2b5850e2e05c396a229197b39afa30811
Ubuntu <ubuntu@ip-172-31-39-114.ap-northeast-2.compute.internal>
2022-06-01 17:43:38 +0000

    All passed!

--- lib/user/syscall.c
+++ lib/user/syscall.c
@@ -1,6 +1,7 @@
 #include <syscall.h>
 #include <stdint.h>
 #include "../syscall-nr.h"
+typedef int pid_t;
 
 __attribute__((always_inline))
 static __inline int64_t syscall (uint64_t num_, uint64_t a1_, uint64_t a2_,
@@ -22,7 +23,7 @@ static __inline int64_t syscall (uint64_t num_, uint64_t a1_, uint64_t a2_,
 			"mov %5, %%r10\n"
 			"mov %6, %%r8\n"
 			"mov %7, %%r9\n"
-			"syscall\n"		// 커널로 전환되는 것?
+			"syscall\n"
 			: "=a" (ret)
 			: "g" (num), "g" (a1), "g" (a2), "g" (a3), "g" (a4), "g" (a5), "g" (a6)
 			: "cc", "memory");
--- threads/thread.c
+++ threads/thread.c
@@ -273,6 +273,21 @@ thread_create (const char *name, int priority,
 	init_thread (t, name, priority);
 	tid = t->tid = allocate_tid ();
 
+	/* 현재 스레드의 자식 리스트에 새로 생성한 스레드 추가 */
+    struct thread *curr = thread_current();
+    list_push_back(&curr->child_list,&t->child_elem);
+
+    /* 파일 디스크립터 초기화 */
+    t->fdTable = palloc_get_multiple(PAL_ZERO,FDT_PAGES);
+    if(t->fdTable == NULL)
+        return TID_ERROR;
+    t->fdIdx = 2;
+    t->fdTable[0] = 1;
+    t->fdTable[1] = 2;
+
+    t->stdin_count = 1;
+    t->stdout_count = 1;
+
 	/* Call the kernel_thread if it scheduled.
 	 * Note) rdi is 1st argument, and rsi is 2nd argument. */
 	t->tf.rip = (uintptr_t) kernel_thread;
@@ -283,6 +298,7 @@ thread_create (const char *name, int priority,
 	t->tf.ss = SEL_KDSEG;
 	t->tf.cs = SEL_KCSEG;
 	t->tf.eflags = FLAG_IF;
+
 
 	/* Add to run queue. */
 	thread_unblock (t);
@@ -540,6 +556,11 @@ init_thread (struct thread *t, const char *name, int priority) {
 	t->init_priority = priority;
 	t->wait_on_lock = NULL;
 	list_init(&t->list_donation);
+	list_init(&t->child_list);
+
+	sema_init(&t->wait_sema,0);
+    sema_init(&t->fork_sema,0);
+    sema_init(&t->free_sema,0);
 
 	t->nice = NICE_DEFAULT;
 	t->recent_cpu = RECENT_CPU_DEFAULT;
