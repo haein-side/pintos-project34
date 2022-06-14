6c5709ce616d9846cd5e48f35f8515b07bcabe86
Yoonwoo <mryoonwoo@naver.com>
2022-06-14 18:14:20 +0000

    supplemental_page_table_kill 구현!

--- vm/vm.c
+++ vm/vm.c
@@ -12,6 +12,8 @@ struct page *page_lookup (struct hash *h, const void *va); /*** haein ***/
 unsigned page_hash (const struct hash_elem *h_elem, void *aux UNUSED);
 bool page_less (const struct hash_elem *h_elem1, const struct hash_elem *h_elem2, void *aux UNUSED);
 
+void spt_hash_destroctor (struct hash_elem *e, void *aux); 	/*** GrilledSalmon ***/
+
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
 void
@@ -260,11 +262,22 @@ supplemental_page_table_copy (struct supplemental_page_table *dst UNUSED,
 		struct supplemental_page_table *src UNUSED) {
 }
 
+/*** GrilledSalmon ***/
+void spt_hash_destroctor (struct hash_elem *e, void *aux) {
+	struct page *page = hash_entry(e, struct page, hash_elem);
+	/* filebacked할 때 수정 필요!!!(writeback) */
+	
+	return vm_dealloc_page(page);
+}
+
+/*** GrilledSalmon ***/
 /* Free the resource hold by the supplemental page table */
 void
-supplemental_page_table_kill (struct supplemental_page_table *spt UNUSED) {
+supplemental_page_table_kill (struct supplemental_page_table *spt) {
 	/* TODO: Destroy all the supplemental_page_table hold by thread and
 	 * TODO: writeback all the modified contents to the storage. */
+	
+	hash_destroy(&spt->h, spt_hash_destroctor);
 }
 
 
