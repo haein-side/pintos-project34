7558a5aaa9c1a672642099609309fbeb76b761fc
Yoonwoo <mryoonwoo@naver.com>
2022-06-15 06:42:10 +0000

    process_exec의 process_cleanup 이후 spt_init 추가

--- userprog/process.c
+++ userprog/process.c
@@ -286,6 +286,10 @@ process_exec (void *f_name) {
 	/* We first kill the current context */
 	process_cleanup ();
 
+	#ifdef VM
+	supplemental_page_table_init (&thread_current()->spt);
+	#endif
+
 	/* argument parsing */
 	char *argv[30];
 	int argc = 0;
